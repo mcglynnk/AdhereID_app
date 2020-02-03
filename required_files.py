5c6d84cf83069559f066d3ae282a287c82b66813
mcglynnk <mcglynnkell@gmail.com>
2020-02-02 20:02:26 -0500

    Push charts version

--- required_files.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-X = pd.read_csv(r'C:\Users\Kelly\Documents\Python\AdhereID_app\files\X.csv', index_col=0)
-plm = pd.read_csv(r'C:\Users\Kelly\Documents\Python\AdhereID_app\files\plm.csv')
-
